728fee99e17b589ba89a5e660000e04457b3bc65
Sascha Krauß <sascha.krauss@outlook.de>
2021-01-12 22:25:48 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import * as http from "http";
+import * as WebSocket from "ws";
+
+// rest of the code remains same
+const app = express();
+
+// initialize a simple http server
+const server = http.createServer(app);
+
+// initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws: WebSocket) => {
+
+    // connection is up, let's add a simple simple event
+    ws.on("message", (message: string) => {
+        wss.clients.forEach((c) => {
+            // tslint:disable-next-line:triple-equals
+            if (c != ws) {
+                c.send(message);
+            }
+        });
+    });
+});
+
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started :)`);
+});
